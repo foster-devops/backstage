2ef12f1cf8cae72a0295f5d73ab50b9c5986f25a
Michael Foster <mifoster1723@gmail.com>
2023-08-11 21:45:58 -0500

    github sso & integrations

---
+++ packages/app/src/components/signin/identityProviders.ts
@@ -0,0 +1,10 @@
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
+
+export const providers = 
+  {
+    id: 'github-auth-provider',
+    title: 'GitHub',
+    message: 'Sign in using GitHub',
+    apiRef: githubAuthApiRef,
+  };
+
